8cf04effeab5e1cc7b6c62e800b2169feda3fe5d
yameholo <ame.aroid.xz5@gmail.com>
2020-05-03 06:09:41 +0900

    Add hash generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use sha1::{Sha1, Digest};
+use std::vec::Vec;
+use std::fs;
+use std::env;
+
+fn calc_hash(body: &String) -> String {
+    let len = body.len();
+    let mut hasher = Sha1::new();
+    hasher.input(format!("commit {}\0{}", len, body));
+    hasher.result()
+        .iter()
+        .map(|i|format!("{:x}",i))
+        .collect::<Vec<_>>()
+        .join("")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let txt = fs::read_to_string(&args[1]).unwrap();
+    println!("{}", calc_hash(&txt));
 }
